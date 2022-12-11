99ba3acea45ccedff577836a63cd186986f5723f
arrusiak <arusminasyan909@gmail.com>
2022-12-12 02:47:52 +0400

    inital commit

---
+++ src/routes/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import AuthController from '../controllers/auth.controller';
+import ProductController from '../controllers/product.controller';
+const router = express.Router();
+import {requireAuth} from '../middlewares';
+import multer from 'multer';
+import path from 'path';
+import {imgFilter} from "../utils/fileUpload";
+import CategoryController from "../controllers/category.controller";
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'storage');
+    },
+    filename(req, file, cb) {
+        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const uploader = multer({
+    storage,
+    fileFilter: imgFilter
+});
+
+
+router.post('/login', AuthController.login);
+router.get('/products', ProductController.getProducts);
+router.get('/products/:id', ProductController.getProduct);
+router.post('/products', [requireAuth, uploader.single('image')], ProductController.create);
+router.patch('/products/:id', [requireAuth, uploader.single('image')], ProductController.update);
+router.delete('/products/:id', [requireAuth], ProductController.delete);
+router.get('/categories', CategoryController.getCategories);
+router.get('/categories/:id', CategoryController.getCategory);
+
+export = router;
